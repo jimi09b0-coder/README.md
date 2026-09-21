47c71051d28b05c43dfb8f270327073068f1d299
jimi09b0-coder <jimi09b0@gmail.com>
2026-09-21 03:12:09 +0100

    Create jobs.js
    Jimi

---
+++ jobs.js
@@ -0,0 +1,46 @@
+export const jobs = [
+  {
+    id: "1",
+    title: "Agent de sécurité",
+    country: "Belgique",
+    city: "Bruxelles",
+    category: "Sécurité",
+    contract: "Temps plein",
+    salary: "Selon l'employeur",
+    description:
+      "Poste dans le domaine de la sécurité et du gardiennage.",
+  },
+  {
+    id: "2",
+    title: "Serveur / Serveuse",
+    country: "Belgique",
+    city: "Bruxelles",
+    category: "Hôtellerie",
+    contract: "Temps plein",
+    salary: "Selon l'employeur",
+    description:
+      "Poste dans un hôtel ou restaurant.",
+  },
+  {
+    id: "3",
+    title: "Réceptionniste hôtel",
+    country: "Belgique",
+    city: "Anvers",
+    category: "Hôtellerie",
+    contract: "Temps plein",
+    salary: "Selon l'employeur",
+    description:
+      "Accueil des clients et gestion de la réception.",
+  },
+  {
+    id: "4",
+    title: "Agent de sécurité",
+    country: "France",
+    city: "Paris",
+    category: "Sécurité",
+    contract: "Temps plein",
+    salary: "Selon l'employeur",
+    description:
+      "Surveillance et protection des personnes et des biens.",
+  }
+];
